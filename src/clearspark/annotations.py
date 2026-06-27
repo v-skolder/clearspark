a58f3210b6265f9678c7cc59d01f4931974324d8
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-06-27 14:00:55 -0300

    fix(annotations): return validated value in dataframe/column validators

--- src/clearspark/annotations.py
+++ src/clearspark/annotations.py
@@ -20,7 +20,11 @@ def _hasattrs(v: Any, required_attr: list[str]) -> bool:
 def validate_spark_session(v: Any) -> Any:
     required_attr = ["read", "createDataFrame", "table", "catalog"]
 
-    if not _hasattrs(v, required_attr) and v is not None:
+    if v is None:
+        # the active `SparkSession` will be used
+        return v
+            
+    if not _hasattrs(v, required_attr):
         raise ValueError(f"Invalid SparkSession-like object: {type(v).__name__}")
 
     return v
@@ -32,12 +36,16 @@ def validate_spark_dataframe(v: Any) -> Any:
     if not _hasattrs(v, required_attr):
         raise ValueError(f"Invalid Dataframe-like object: {type(v).__name__}")
 
+    return v
+
 
 def validate_spark_column(v: Any) -> Any:
     required_attr = ["alias", "cast", "desc", "asc"]
 
     if not _hasattrs(v, required_attr):
         raise ValueError(f"Invalid Column-like object: {type(v).__name__}")
+
+    return v
 
 # ANNOTATIONS
 
